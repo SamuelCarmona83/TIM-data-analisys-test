3c051efccadd11f16c83ebfdb097284f20ce14bb
SamuelCarmona83 <samuel.carmona.rodrigz@gmail.com>
2023-08-22 23:45:21 -0400

    :tada: transacciones no cruzadas de tabla bansur

---
+++ scripts/calculate_not_cross.py
@@ -0,0 +1,44 @@
+import psycopg2
+from psycopg2 import extras
+
+# Parámetros de la base de datos
+db_params = {
+    "dbname": "mydb",
+    "user": "myuser",
+    "password": "mypassword",
+    "host": "localhost",
+    "port": "5432"
+}
+
+# Consulta para obtener transacciones cruzadas entre CLAP y BANSUR
+query_crossed_transactions = """
+    SELECT
+        COUNT(*) AS crossed_count
+    FROM clap c
+    JOIN bansur b ON c.INICIO6_TARJETA || c.FINAL4_TARJETA = b.TARJETA
+                 AND c.MONTO = b.MONTO
+                 AND c.FECHA_TRANSACCION = b.FECHA_TRANSACCION
+"""
+
+with psycopg2.connect(**db_params) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute(query_crossed_transactions)
+        crossed_count = cursor.fetchone()[0]
+
+# Consulta para obtener el total de transacciones en la tabla CLAP
+query_total_clap_transactions = """
+    SELECT COUNT(*) FROM clap
+"""
+
+with psycopg2.connect(**db_params) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute(query_total_clap_transactions)
+        total_clap_transactions = cursor.fetchone()[0]
+
+if total_clap_transactions > 0:
+    porcentaje_cruzadas = (crossed_count / total_clap_transactions) * 100
+    print(f"Porcentaje de transacciones no cruzadas: {100-porcentaje_cruzadas:.2f}%")
+else:
+    print("No hay transacciones en la tabla CLAP para calcular el porcentaje.")
+
+
